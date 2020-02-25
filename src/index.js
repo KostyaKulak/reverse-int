f9a5ff166f64e86cfb079b525293290654a26c03
Kanstantsin Kulak <kkulak24@gmail.com>
2020-02-25 15:23:33 +0300

    feat: implement reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var a = Math.abs(n), i = (Math.log(a) * Math.LOG10E + 1 | 0) - 1, res = 0;
+    while (a != 0) {
+        res += a % 10 * Math.pow(10, i);
+        a = a / 10 >> 0;
+        i--;
+    }
+    return res
 }
